a7646c140ee52a98cd6f77b6f9c06e373d903234
Rita Reyes <rita.303015@gmail.com>
2022-02-17 17:55:24 -0600

    primer_commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+// cambio para el repositorio remoto
+#include "Cliente.cpp"
+#include <iostream>
+using namespace std;
+main(){
+	string nit,nombres,apellidos,direccion;
+	int telefono;
+	cout<<"Ingrese Nit: ";
+	cin>>nit;
+	cout<<"Ingrese Nombres: ";
+	cin>>nombres;
+	cout<<"Ingrese Apellidos: ";
+	cin>>apellidos;
+	cout<<"Ingrese Direccion: ";
+	cin>>direccion;
+	cout<<"Ingrese Telefono: ";
+	cin>>telefono;
+	// instanciar un objeto
+	Cliente obj = Cliente(nombres,apellidos,direccion,telefono,nit);
+	obj.mostrar();
+	cout<<"Ingrese Nit: ";
+	cin>>nit;
+	obj.setNit(nit);
+	obj.mostrar();
+	// instanciar un objeto y utilizar get and set
+	
+/*	Cliente obj = Cliente();
+	obj.setNit(nit);
+	obj.setNombres(nombres);
+	obj.setApellidos(apellidos);
+	obj.setDireccion(direccion);
+	obj.setTelefono(telefono);
+	//obj.mostrar();
+	cout<<"Nit: "<<obj.getNit()<<endl;
+	cout<<"Nombres: "<<obj.getNombres()<<endl;
+	cout<<"Apellidos: "<<obj.getApellidos()<<endl;
+	cout<<"Direccion: "<<obj.getDireccion()<<endl;
+	cout<<"Telefono: "<<obj.getTelefono()<<endl;
+*/	
+}
